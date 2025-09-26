39d419152db7e60ad4b5a86a454c03b7a0f0b45a
Davyd Krykota <krikotadavid957@gmail.com>
2025-09-26 21:01:58 +0300

    integrate basic db functionality with prisma for users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,45 @@
-import { Injectable } from '@nestjs/common';
-import { UserDto } from '../dtos/user.dto';
+import { HttpException, Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
-  private users: UserDto[] = [];
+  constructor(private prisma: PrismaService) {}
 
-  findAll(): UserDto[] {
+  createUser(data: Prisma.UserCreateInput) {
+    return this.prisma.user.create({ data });
+  }
+
+  getUsers() {
+    return this.prisma.user.findMany();
+  }
+  getUserById(id: number) {
+    return this.prisma.user.findUnique({where: {id}})
+  }
+
+  async updateUserById(id: number, data: Prisma.UserUpdateInput) {
+    const findUser = await this.getUserById(id);
+    if(!findUser) throw new HttpException('User Not Found',404);
+
+    if(data.email) {
+      const findUser = await this.prisma.user.findUnique({where: {email: data.email as string}})
+
+      if(findUser) throw new HttpException('Email already taken', 400)
+    }
+    return this.prisma.user.update({ where: {id}, data} );
+  }
+  async deleteUserById(id: number) {
+    const findUser = await this.getUserById(id);
+    if(!findUser) throw new HttpException("User Not Found", 404);
+    return this.prisma.user.delete({where:{ id }})
+  }
+}
+
+
+
+/*
+  private users: UserDto[] = [];
+ findAll(): UserDto[] {
     return this.users;
   }
 
@@ -15,15 +49,16 @@ export class UsersService {
   }
 
   updateUser(id: number, updatedUser: UserDto): UserDto | undefined {
-    const index = this.users.findIndex(user => user.id === id)
-    if(index > -1) {
+    const index = this.users.findIndex((user) => user.id === id);
+    if (index > -1) {
       this.users[index] = { ...updatedUser, id };
       return this.users[index];
     }
     return undefined;
   }
 
-  deleteUser(id: number): void{
-    this.users = this.users.filter(user => user.id !== id)
+  deleteUser(id: number): void {
+    this.users = this.users.filter((user) => user.id !== id);
   }
 }
+*/
