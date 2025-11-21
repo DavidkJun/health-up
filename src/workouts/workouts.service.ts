a4cbd0d532943716841e8a6323054a166daae7dd
Davyd Krykota <krikotadavid957@gmail.com>
2025-11-21 13:14:07 +0200

    feat: add workout service

---
+++ src/workouts/workouts.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, HttpException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export class WorkoutsService {
+  constructor(private prisma: PrismaService) {
+  }
+
+  createWorkout(data: Prisma.WorkoutCreateInput) {
+    return this.prisma.workout.create({ data });
+  }
+
+  getWorkouts(userId: number) {
+    return this.prisma.workout.findMany(
+      {where: {userId: userId},
+        include: {
+          exercises: true
+        }});
+  }
+
+  getWorkoutById(id: number, userId: number) {
+    return this.prisma.workout.findFirst(
+      {where: {id: id, userId: userId},
+      include: {
+        exercises: true
+      }});
+  }
+
+  async updateWorkoutById(id: number, data: Prisma.WorkoutUpdateInput, userId: number) {
+    const findWorkout = await this.getWorkoutById(id, userId);
+    if(!findWorkout) throw new HttpException('Workout Not Found',404);
+
+    return this.prisma.workout.update({ where: {id: id}, data} );
+  }
+
+  async deleteWorkout(id: number, userId: number) {
+    const findWorkout = await this.getWorkoutById(id, userId);
+    if(!findWorkout) throw new HttpException("Workout Not Found", 404);
+    return this.prisma.workout.delete({where:{ id }})
+  }
+}
